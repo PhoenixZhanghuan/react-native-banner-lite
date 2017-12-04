ef12c56a2788c8489b5235f36411150b70decdce
PhoenixZhang123 <2387749855@qq.com>
2017-12-04 14:45:08 +0800

    修改banner样式

---
+++ README.md
@@ -0,0 +1,42 @@
+# react-native-banner-lite
+A banner component with adsorption. Compatible with iOS and Android.
+
+        <BannerLite
+          mainTitle={styles.mainTitle}
+          subTitle={styles.subTitle}
+          image={styles.image}
+          items={[
+            {
+              title: "内旋不足的你, 该如何“科学”跑步",
+              subtitle: "身为跑者不可不知的这些基本法则",
+              imageURL: "http://imagscdn.3234.com/pictures/0/01/r4UhVf.jpg",
+              onPress:(index)=>{console.log("tap"+index)}
+            },
+            {
+              title: "内旋不足的你, 该如何“科学”跑步",
+              subtitle: "身为跑者不可不知的这些基本法则",
+              imageURL: "http://pic69.nipic.com/file/20150610/21067407_235515103000_2.jpg",
+              onPress:(index)=>{console.log("tap"+index)}
+            },
+            {
+              title: "内旋不足的你, 该如何“科学”跑步",
+              subtitle: "身为跑者不可不知的这些基本法则",
+              imageURL: "http://a4.att.hudong.com/35/64/01300000276819133197645554930.jpg",
+              onPress:(index)=>{console.log("tap"+index)}
+            },
+            {
+              title: "内旋不足的你, 该如何“科学”跑步",
+              subtitle: "身为跑者不可不知的这些基本法则",
+              imageURL: "http://pic.58pic.com/58pic/12/56/90/04A58PICsKy.jpg",
+              onPress:(index)=>{console.log("tap"+index)}
+            },
+            {
+              title: "内旋不足的你, 该如何“科学”跑步",
+              subtitle: "身为跑者不可不知的这些基本法则",
+              imageURL: "http://h.hiphotos.baidu.com/image/h%3D200/sign=3a225a4129a4462361caa262a8227246/30adcbef76094b36fbaf3bd6aacc7cd98d109dcf.jpg",
+              onPress:(index)=>{console.log("tap"+index)}
+            },
+          ]}
+        />
+        
+        
---
+++ index.js
@@ -0,0 +1,160 @@
+import React, {
+  Component,
+} from 'react';
+
+import {
+  PropTypes,
+  StyleSheet,
+  ScrollView,
+  View,
+  Dimensions,
+  Text,
+  Image,
+  TouchableOpacity,
+} from 'react-native';
+
+export default class DiscussBannerScrollView extends Component {
+
+	static propTypes = {
+		items: React.PropTypes.arrayOf(React.PropTypes.shape({
+      title: React.PropTypes.string,
+			subtitle: React.PropTypes.string,
+			imageURL: React.PropTypes.string,
+			onPress: React.PropTypes.func,
+		})),
+	};
+
+	constructor(props) {
+	  super(props);
+
+	  this.state = {
+	  	items: this.props.items,
+	  };
+	}
+
+	onScrollEndDrag = e => {
+    const { contentOffset } = e.nativeEvent;
+
+    if (contentOffset.x < (this.state.items.length-1.8) * (260+15) && (this.state.items.length > 1)) {
+	    if (contentOffset.x%(260+15) < (265+15)/2 ) {
+	    	this.refs.scrollView.scrollTo({x: contentOffset.x-contentOffset.x%(260+15), y: 0, animated: true});
+	    } else {
+	    	this.refs.scrollView.scrollTo({x: (contentOffset.x-contentOffset.x%(260+15))+(260+15), y: 0, animated: true});
+	    }
+	  } else {
+	  	if (contentOffset.x >= (this.state.items.length-1.8) * (260+15) && (this.state.items.length > 1)) {
+	  		this.refs.scrollView.scrollTo({x: (this.state.items.length*(260+15) - Dimensions.get('window').width)+15, y: 0, animated: true});
+	  	} else if (this.state.items.length <= 1) {
+	  		this.refs.scrollView.scrollTo({x: 0, y: 0, animated: true});
+	  	}
+	  }
+  }
+
+	render() {
+
+		var bannerItems = [];
+
+		for (let i = 0; i < this.state.items.length; i++) {
+			bannerItems.push(
+				<BannerItem
+					key={"bannerItem"+i}
+					index={i}
+          mainTitleStyle={this.props.mainTitle}
+          subTitleStyle={this.props.subTitle}
+          imageStyle={this.props.image}
+          mainTitle={this.state.items[i].title}
+          subTitle={this.state.items[i].subtitle}
+					imageURL={this.state.items[i].imageURL}
+					onPress={this.state.items[i].onPress}
+				/>
+			);
+		}
+
+		if (bannerItems.length === 0) {
+			bannerItems.push(
+				<View key='empty' style={[itemStyles.containerFirst, itemStyles.itemSize]}>
+					<Text>
+						这里什么也没有
+					</Text>
+				</View>
+			);
+		}
+
+		if (bannerItems.length === 1) {
+			bannerItems.push(
+				<View key='holder' style={[itemStyles.container, itemStyles.itemSize, itemStyles.emptyItem]}/>
+			);
+		}
+
+		return(
+			<ScrollView
+				ref='scrollView'
+				centerContent={false}
+				onScrollEndDrag={this.onScrollEndDrag}
+				contentContainerStyle={scrollViewStyles.scrollView}
+				horizontal={true}
+				showsHorizontalScrollIndicator={false}
+			>
+				{bannerItems}
+			</ScrollView>
+		);
+	}
+
+}
+
+class BannerItem extends Component {
+
+	constructor(props) {
+	  super(props);
+	}
+
+	render() {
+		return (
+			<TouchableOpacity style={(this.props.index===0)?itemStyles.containerFirst:itemStyles.container} onPress={() => this.props.onPress(this.props.index)}>
+				<Image
+	        style={this.props.imageStyle}
+	        source={{uri: this.props.imageURL}}
+	      />
+        <Text numberOfLines={1} style={this.props.mainTitleStyle}>{this.props.mainTitle}</Text>
+        <Text numberOfLines={1} style={this.props.subTitleStyle}>{this.props.subTitle}</Text>
+			</TouchableOpacity>
+		);
+	}
+}
+
+const itemStyles = StyleSheet.create({
+	emptyItem: {
+		backgroundColor: 'transparent',
+	},
+	container: {
+		marginRight: 15,
+	},
+	containerFirst: {
+		marginRight: 15,
+		marginLeft: 15,
+	},
+  mainTitle: {
+	  width: 260,
+	  fontSize: 15,
+    fontWeight: 'bold',
+    color: '#000000',
+    marginTop: 15
+  },
+  subTitle: {
+    width: 260,
+	  fontSize: 12,
+    color: '#666666',
+    marginTop: 10
+  },
+	image: {
+		height: 195,
+		width: 260,
+	}
+});
+
+const scrollViewStyles = StyleSheet.create({
+	scrollView: {
+		flexDirection: 'row',
+		alignItems: 'center',
+	},
+});
